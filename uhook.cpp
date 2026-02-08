edf7507e938c7f71836a7387096eb43068b67d2b
karan283861 <190244533+karan283861@users.noreply.github.com>
2026-02-08 16:52:33 +1100

    Remove PLOG statements and refactor to use return codes

--- uhook.cpp
+++ uhook.cpp
@@ -19,14 +19,14 @@ std::string GetIndentedTabString(void)
 ProcessEventPrototype original_processevent{reinterpret_cast<ProcessEventPrototype>(0)};
 UFunctionHooks<ProcessEventPrototype> processevent_hooks(nullptr);
 void __fastcall ProcessEventHook(UObject *calling_uobject,
-								 void *unused, UFunction *calling_ufunction,
-								 void *parameters, void *result)
+								 UFunction *calling_ufunction,
+								 void *parameters)
 {
 #if defined(_DEBUG)
 	indent_level++;
 	PLOG_VERBOSE << GetIndentedTabString() << std::format("{0} : {1}", calling_ufunction->GetFullName(), calling_uobject->GetFullName());
 #endif
-	processevent_hooks.ExecuteHook(calling_ufunction, calling_uobject, unused, calling_ufunction, parameters, result);
+	processevent_hooks.ExecuteHook(calling_ufunction, calling_uobject, calling_ufunction, parameters);
 #if defined(_DEBUG)
 	indent_level--;
 #endif
@@ -35,7 +35,7 @@ void __fastcall ProcessEventHook(UObject *calling_uobject,
 ProcessInternalPrototype original_processinternal{reinterpret_cast<ProcessInternalPrototype>(0)};
 UFunctionHooks<ProcessInternalPrototype> processinternal_hooks(nullptr);
 void __fastcall ProcessInternalHook(UObject *calling_uobject,
-									void *unused, FFrame &stack,
+									FFrame &stack,
 									void *result)
 {
 	auto calling_ufunction = reinterpret_cast<UFunction *>(stack.node_);
@@ -43,7 +43,24 @@ void __fastcall ProcessInternalHook(UObject *calling_uobject,
 	indent_level++;
 	PLOG_VERBOSE << GetIndentedTabString() << std::format("{0} : {1}", calling_ufunction->GetFullName(), calling_uobject->GetFullName());
 #endif
-	processinternal_hooks.ExecuteHook(calling_ufunction, calling_uobject, unused, stack, result);
+	processinternal_hooks.ExecuteHook(calling_ufunction, calling_uobject, stack, result);
+#if defined(_DEBUG)
+	indent_level--;
+#endif
+}
+
+InvokePrototype original_invoke{reinterpret_cast<InvokePrototype>(0)};
+UFunctionHooks<InvokePrototype> invoke_hooks(nullptr);
+void __fastcall InvokeHook(UFunction *calling_ufunction,
+						   UObject *calling_uobject,
+						   FFrame &stack,
+						   void *result)
+{
+#if defined(_DEBUG)
+	indent_level++;
+	PLOG_VERBOSE << GetIndentedTabString() << std::format("{0}. {1} : {2}", indent_level, calling_ufunction->GetFullName(), calling_uobject->GetFullName());
+#endif
+	invoke_hooks.ExecuteHook(calling_ufunction, calling_ufunction, calling_uobject, stack, result);
 #if defined(_DEBUG)
 	indent_level--;
 #endif
@@ -52,23 +69,22 @@ void __fastcall ProcessInternalHook(UObject *calling_uobject,
 CallFunctionPrototype original_callfunction{reinterpret_cast<CallFunctionPrototype>(0)};
 UFunctionHooks<CallFunctionPrototype> callfunction_hooks(nullptr);
 void __fastcall CallFunctionHook(UObject *calling_uobject,
-								 void *unused, FFrame &stack,
+								 FFrame &stack,
 								 void *result, UFunction *calling_ufunction)
 {
 	if (!callfunction_hooks.original_function_)
 	{
-		return original_callfunction(calling_uobject, unused, stack, result, calling_ufunction);
+		return original_callfunction(calling_uobject, stack, result, calling_ufunction);
 	}
 
 #if defined(_DEBUG)
 	static constexpr unsigned int kFUNC_Native{0x00000400};
-	auto is_native{calling_ufunction->iNative};
+	auto is_native{false};
 	auto is_funcnative{calling_ufunction->FunctionFlags & kFUNC_Native};
 	indent_level++;
-	PLOG_VERBOSE << GetIndentedTabString() << std::format("{0} {1} : {2}", is_native ? "[NATIVE]" : (is_funcnative ? "[FUNC_Native]" : ""), calling_ufunction->GetFullName(), calling_uobject->GetFullName());
+	PLOG_VERBOSE << GetIndentedTabString() << std::format("{0}. {1} {2} : {3}", indent_level, is_native ? "[NATIVE]" : (is_funcnative ? "[FUNC_Native]" : ""), calling_ufunction->GetFullName(), calling_uobject->GetFullName());
 #endif
-	callfunction_hooks.ExecuteHook(calling_ufunction, calling_uobject, unused, stack, result, calling_ufunction);
-
+	callfunction_hooks.ExecuteHook(calling_ufunction, calling_uobject, stack, result, calling_ufunction);
 #if defined(_DEBUG)
 	indent_level--;
 #endif
