7221b7d1c6a3037219673dee57de2a593ffe5f9f
karan283861 <190244533+karan283861@users.noreply.github.com>
2026-03-24 00:46:41 +1100

    Consolidate UE3, UE4 and UE5 functionality all into one

--- uhook.cpp
+++ uhook.cpp
@@ -16,63 +16,176 @@ std::string GetIndentedTabString(void)
 }
 #endif
 
-ProcessEventPrototype original_processevent{reinterpret_cast<ProcessEventPrototype>(0)};
-UFunctionHooks<ProcessEventPrototype> processevent_hooks{};
-void __fastcall ProcessEventHook(UObject *calling_uobject,
-								 void *unused, UFunction *calling_ufunction,
-								 void *parameters, void *result)
+namespace UE3
 {
-#if defined(_DEBUG)
-	indent_level++;
-	PLOG_VERBOSE << GetIndentedTabString() << std::format("{0} : {1}", calling_ufunction->GetFullName(), calling_uobject->GetFullName());
-#endif
-	if (processevent_hooks.ExecuteHook(calling_ufunction, calling_uobject, unused, calling_ufunction, parameters, result) == ExecuteHookResult::kFailedNoOriginalFunctionFound)
+	void __fastcall ProcessEventHook(UObject *calling_uobject,
+									 void *unused, UFunction *calling_ufunction,
+									 void *parameters, void *result)
 	{
-		original_processevent(calling_uobject, unused, calling_ufunction, parameters, result);
-	}
 #if defined(_DEBUG)
-	indent_level--;
+		indent_level++;
+		log_function(std::format("{0}{1} : {2}", GetIndentedTabString(), calling_ufunction->GetFullName(), calling_uobject->GetFullName()));
 #endif
+		if (processevent_hooks.ExecuteHook(calling_ufunction, calling_uobject, unused, calling_ufunction, parameters, result) == ExecuteHookResult::kFailedNoOriginalFunctionFound)
+		{
+			original_processevent(calling_uobject, unused, calling_ufunction, parameters, result);
+		}
+#if defined(_DEBUG)
+		indent_level--;
+#endif
+	}
+
+	void __fastcall ProcessInternalHook(UObject *calling_uobject,
+										void *unused, FFrame &stack,
+										void *result)
+	{
+		auto calling_ufunction = reinterpret_cast<UFunction *>(stack.node_);
+#if defined(_DEBUG)
+		indent_level++;
+		log_function(std::format("{0}{1} : {2}", GetIndentedTabString(), calling_ufunction->GetFullName(), calling_uobject->GetFullName()));
+#endif
+		if (processinternal_hooks.ExecuteHook(calling_ufunction, calling_uobject, unused, stack, result) == ExecuteHookResult::kFailedNoOriginalFunctionFound)
+		{
+			original_processinternal(calling_uobject, unused, stack, result);
+		}
+#if defined(_DEBUG)
+		indent_level--;
+#endif
+	}
+
+	void __fastcall CallFunctionHook(UObject *calling_uobject,
+									 void *unused, FFrame &stack,
+									 void *result, UFunction *calling_ufunction)
+	{
+#if defined(_DEBUG)
+		static constexpr unsigned int kFUNC_Native{0x00000400};
+		auto is_native{calling_ufunction->iNative};
+		auto is_funcnative{calling_ufunction->FunctionFlags & kFUNC_Native};
+		indent_level++;
+		log_function(std::format("{0}{1} {2} : {3}", GetIndentedTabString(), is_native ? "[NATIVE]" : (is_funcnative ? "[FUNC_Native]" : ""), calling_ufunction->GetFullName(), calling_uobject->GetFullName()));
+#endif
+		if (callfunction_hooks.ExecuteHook(calling_ufunction, calling_uobject, unused, stack, result, calling_ufunction) == ExecuteHookResult::kFailedNoOriginalFunctionFound)
+		{
+			original_callfunction(calling_uobject, unused, stack, result, calling_ufunction);
+		}
+#if defined(_DEBUG)
+		indent_level--;
+#endif
+	}
 }
 
-ProcessInternalPrototype original_processinternal{reinterpret_cast<ProcessInternalPrototype>(0)};
-UFunctionHooks<ProcessInternalPrototype> processinternal_hooks{};
-void __fastcall ProcessInternalHook(UObject *calling_uobject,
-									void *unused, FFrame &stack,
-									void *result)
+namespace UE4
 {
-	auto calling_ufunction = reinterpret_cast<UFunction *>(stack.node_);
-#if defined(_DEBUG)
-	indent_level++;
-	PLOG_VERBOSE << GetIndentedTabString() << std::format("{0} : {1}", calling_ufunction->GetFullName(), calling_uobject->GetFullName());
-#endif
-	if (processinternal_hooks.ExecuteHook(calling_ufunction, calling_uobject, unused, stack, result) == ExecuteHookResult::kFailedNoOriginalFunctionFound)
+	void __fastcall ProcessEventHook(UObject *calling_uobject,
+									 UFunction *calling_ufunction,
+									 void *parameters)
 	{
-		original_processinternal(calling_uobject, unused, stack, result);
-	}
 #if defined(_DEBUG)
-	indent_level--;
+		indent_level++;
+		log_function(std::format("{0}{1} : {2}", GetIndentedTabString(), calling_ufunction->GetFullName(), calling_uobject->GetFullName()));
 #endif
+		if (processevent_hooks.ExecuteHook(calling_ufunction, calling_uobject, calling_ufunction, parameters) == ExecuteHookResult::kFailedNoOriginalFunctionFound)
+		{
+			original_processevent(calling_uobject, calling_ufunction, parameters);
+		}
+#if defined(_DEBUG)
+		indent_level--;
+#endif
+	}
+
+	void __fastcall ProcessInternalHook(UObject *calling_uobject,
+										FFrame &stack,
+										void *result)
+	{
+		auto calling_ufunction = reinterpret_cast<UFunction *>(stack.node_);
+#if defined(_DEBUG)
+		indent_level++;
+		log_function(std::format("{0}{1} : {2}", GetIndentedTabString(), calling_ufunction->GetFullName(), calling_uobject->GetFullName()));
+#endif
+		if (processinternal_hooks.ExecuteHook(calling_ufunction, calling_uobject, stack, result) == ExecuteHookResult::kFailedNoOriginalFunctionFound)
+		{
+			original_processinternal(calling_uobject, stack, result);
+		}
+#if defined(_DEBUG)
+		indent_level--;
+#endif
+	}
+
+	void __fastcall CallFunctionHook(UObject *calling_uobject,
+									 FFrame &stack,
+									 void *result, UFunction *calling_ufunction)
+	{
+#if defined(_DEBUG)
+		static constexpr unsigned int kFUNC_Native{0x00000400};
+		auto is_native{calling_ufunction->iNative};
+		auto is_funcnative{calling_ufunction->FunctionFlags & kFUNC_Native};
+		indent_level++;
+		log_function(std::format("{0}{1} {2} : {3}", GetIndentedTabString(), is_native ? "[NATIVE]" : (is_funcnative ? "[FUNC_Native]" : ""), calling_ufunction->GetFullName(), calling_uobject->GetFullName()));
+#endif
+		if (callfunction_hooks.ExecuteHook(calling_ufunction, calling_uobject, stack, result, calling_ufunction) == ExecuteHookResult::kFailedNoOriginalFunctionFound)
+		{
+			original_callfunction(calling_uobject, stack, result, calling_ufunction);
+		}
+#if defined(_DEBUG)
+		indent_level--;
+#endif
+	}
 }
 
-CallFunctionPrototype original_callfunction{reinterpret_cast<CallFunctionPrototype>(0)};
-UFunctionHooks<CallFunctionPrototype> callfunction_hooks{};
-void __fastcall CallFunctionHook(UObject *calling_uobject,
-								 void *unused, FFrame &stack,
-								 void *result, UFunction *calling_ufunction)
+namespace UE5
 {
-#if defined(_DEBUG)
-	static constexpr unsigned int kFUNC_Native{0x00000400};
-	auto is_native{calling_ufunction->iNative};
-	auto is_funcnative{calling_ufunction->FunctionFlags & kFUNC_Native};
-	indent_level++;
-	PLOG_VERBOSE << GetIndentedTabString() << std::format("{0} {1} : {2}", is_native ? "[NATIVE]" : (is_funcnative ? "[FUNC_Native]" : ""), calling_ufunction->GetFullName(), calling_uobject->GetFullName());
-#endif
-	if (callfunction_hooks.ExecuteHook(calling_ufunction, calling_uobject, unused, stack, result, calling_ufunction) == ExecuteHookResult::kFailedNoOriginalFunctionFound)
+	void __fastcall ProcessEventHook(UObject *calling_uobject,
+									 UFunction *calling_ufunction,
+									 void *parameters)
 	{
-		original_callfunction(calling_uobject, unused, stack, result, calling_ufunction);
-	}
 #if defined(_DEBUG)
-	indent_level--;
+		indent_level++;
+		log_function(std::format("{0}{1} : {2}", GetIndentedTabString(), calling_ufunction->GetFullName(), calling_uobject->GetFullName()));
 #endif
+		if (processevent_hooks.ExecuteHook(calling_ufunction, calling_uobject, calling_ufunction, parameters) == ExecuteHookResult::kFailedNoOriginalFunctionFound)
+		{
+			original_processevent(calling_uobject, calling_ufunction, parameters);
+		}
+#if defined(_DEBUG)
+		indent_level--;
+#endif
+	}
+
+	void __fastcall ProcessInternalHook(UObject *calling_uobject,
+										FFrame &stack,
+										void *result)
+	{
+		auto calling_ufunction = reinterpret_cast<UFunction *>(stack.node_);
+#if defined(_DEBUG)
+		indent_level++;
+		log_function(std::format("{0}{1} : {2}", GetIndentedTabString(), calling_ufunction->GetFullName(), calling_uobject->GetFullName()));
+#endif
+		if (processinternal_hooks.ExecuteHook(calling_ufunction, calling_uobject, stack, result) == ExecuteHookResult::kFailedNoOriginalFunctionFound)
+		{
+			original_processinternal(calling_uobject, stack, result);
+		}
+#if defined(_DEBUG)
+		indent_level--;
+#endif
+	}
+
+	void __fastcall CallFunctionHook(UObject *calling_uobject,
+									 FFrame &stack,
+									 void *result, UFunction *calling_ufunction)
+	{
+#if defined(_DEBUG)
+		static constexpr unsigned int kFUNC_Native{0x00000400};
+		auto is_native{calling_ufunction->iNative};
+		auto is_funcnative{calling_ufunction->FunctionFlags & kFUNC_Native};
+		indent_level++;
+		log_function(std::format("{0}{1} {2} : {3}", GetIndentedTabString(), is_native ? "[NATIVE]" : (is_funcnative ? "[FUNC_Native]" : ""), calling_ufunction->GetFullName(), calling_uobject->GetFullName()));
+#endif
+		if (callfunction_hooks.ExecuteHook(calling_ufunction, calling_uobject, stack, result, calling_ufunction) == ExecuteHookResult::kFailedNoOriginalFunctionFound)
+		{
+			original_callfunction(calling_uobject, stack, result, calling_ufunction);
+		}
+#if defined(_DEBUG)
+		indent_level--;
+#endif
+	}
 }
--- uhook.hpp
+++ uhook.hpp
@@ -1,56 +1,13 @@
 #pragma once
 
 #include <format>
-#include <unordered_map>
 #include <vector>
-#include <plog/Log.h>
+#include <string>
+#include <functional>
 #include <SdkHeaders.h>
 
 constexpr size_t kSizeOfUFunctionInternalIndex{2000000};
 constexpr size_t kMaxHookCountPerUFunction{1};
-
-using ProcessEventPrototype = void(__fastcall *)(UObject *calling_uobject,
-												 void *unused, UFunction *calling_ufunction,
-												 void *parameters, void *result);
-
-void __fastcall ProcessEventHook(UObject *calling_uobject,
-								 void *unused, UFunction *calling_ufunction,
-								 void *parameters, void *result);
-
-extern ProcessEventPrototype original_processevent;
-
-class FFrame
-{
-public:
-	void *vmt_{};
-	char unknown_data_00_[12]{};
-	UStruct *node_{};
-	UObject *object_{};
-	unsigned char *code_{};
-	char *locals_{};
-	int line_nums_{};
-	FFrame *previous_frame_{};
-};
-
-using ProcessInternalPrototype = void(__fastcall *)(UObject *calling_uobject,
-													void *unused, FFrame &stack,
-													void *result);
-
-void __fastcall ProcessInternalHook(UObject *calling_uobject,
-									void *unused, FFrame &stack,
-									void *result);
-
-extern ProcessInternalPrototype original_processinternal;
-
-using CallFunctionPrototype = void(__fastcall *)(UObject *calling_uobject,
-												 void *unused, FFrame &stack,
-												 void *result, UFunction *calling_ufunction);
-
-void __fastcall CallFunctionHook(UObject *calling_uobject,
-								 void *unused, FFrame &stack,
-								 void *result, UFunction *calling_ufunction);
-
-extern CallFunctionPrototype original_callfunction;
 
 enum class FunctionHookType
 {
@@ -237,9 +194,209 @@ private:
 	}
 };
 
-extern UFunctionHooks<ProcessEventPrototype> processevent_hooks;
-extern UFunctionHooks<ProcessInternalPrototype> processinternal_hooks;
-extern UFunctionHooks<CallFunctionPrototype> callfunction_hooks;
+template <typename UFunctionHookInformation>
+std::string ValidateUFunctionHookResult(const HookResult &hook_result, const UFunctionHookInformation &hook_information)
+{
+	const auto &name{hook_information.name_};
+	const auto &is_absorbing{hook_information.hook_absorb_ == FunctionHookAbsorb::kAbsorb};
+	switch (hook_result)
+	{
+	case HookResult::kSuccess:
+	{
+		return std::format("Successfully hooked {0}{1}", name, is_absorbing ? " [ABSORBING]" : "");
+		break;
+	}
+	case HookResult::kFailedIncorrectHookTypeAndHookAbsorb:
+	{
+		return std::format("Failed to hook {0} due to incorrect hook type and hook absorb", name);
+		break;
+	}
+	case HookResult::kFailedToFindUFunction:
+	{
+		return std::format("Failed to hook {0} as the UFunction was not found", name);
+		break;
+	}
+	case HookResult::kFailedUFunctionOutOfBounds:
+	{
+		return std::format("Failed to hook {0} as the UFunction index was out of bounds", name);
+		break;
+	}
+	case HookResult::kFailedOverMaxHookCount:
+	{
+		return std::format("Failed to hook {0} as the UFunction already has maximum number of hooks", name);
+		break;
+	}
+	case HookResult::kFailedUnknownHookType:
+	{
+		return std::format("Failed to hook {0} due to unknown hook type", name);
+		break;
+	}
+	default:
+	{
+		return std::format("Hooking {0} resulted in unhandled behaviour", name);
+		break;
+	}
+	}
+	return "Unhandled error";
+}
 
-#define PROCESSEVENT_HOOK(function_hook_name) void __fastcall function_hook_name(UObject *calling_uobject, void *unused, UFunction *calling_ufunction, void *parameters, void *result)
-#define PROCESSINTERNAL_HOOK(function_hook_name) void __fastcall function_hook_name(UObject *calling_uobject, void *unused, FFrame &stack, void *result)
+inline std::function<void(const std::string &)> log_function{};
+
+namespace UE3
+{
+	using ProcessEventPrototype = void(__fastcall *)(UObject *calling_uobject,
+													 void *unused, UFunction *calling_ufunction,
+													 void *parameters, void *result);
+
+	void __fastcall ProcessEventHook(UObject *calling_uobject,
+									 void *unused, UFunction *calling_ufunction,
+									 void *parameters, void *result);
+
+	inline ProcessEventPrototype original_processevent{};
+
+	class FFrame
+	{
+	public:
+		void *vmt_{};
+		char unknown_data_00_[12]{};
+		UStruct *node_{};
+		UObject *object_{};
+		unsigned char *code_{};
+		char *locals_{};
+		int line_nums_{};
+		FFrame *previous_frame_{};
+	};
+
+	using ProcessInternalPrototype = void(__fastcall *)(UObject *calling_uobject,
+														void *unused, FFrame &stack,
+														void *result);
+
+	void __fastcall ProcessInternalHook(UObject *calling_uobject,
+										void *unused, FFrame &stack,
+										void *result);
+
+	inline ProcessInternalPrototype original_processinternal{};
+
+	using CallFunctionPrototype = void(__fastcall *)(UObject *calling_uobject,
+													 void *unused, FFrame &stack,
+													 void *result, UFunction *calling_ufunction);
+
+	void __fastcall CallFunctionHook(UObject *calling_uobject,
+									 void *unused, FFrame &stack,
+									 void *result, UFunction *calling_ufunction);
+
+	inline CallFunctionPrototype original_callfunction{};
+
+	inline UFunctionHooks<ProcessEventPrototype> processevent_hooks{};
+	inline UFunctionHooks<ProcessInternalPrototype> processinternal_hooks{};
+	inline UFunctionHooks<CallFunctionPrototype> callfunction_hooks{};
+}
+
+namespace UE4
+{
+	using ProcessEventPrototype = void(__fastcall *)(UObject *calling_uobject,
+													 UFunction *calling_ufunction,
+													 void *parameters);
+
+	void __fastcall ProcessEventHook(UObject *calling_uobject,
+									 UFunction *calling_ufunction,
+									 void *parameters);
+
+	inline ProcessEventPrototype original_processevent{};
+
+	class FFrame
+	{
+	public:
+		void *vmt_{};
+		char unknown_data_00_[8]{};
+		UStruct *node_{};
+		UObject *object_{};
+		unsigned char *code_{};
+		char *locals_{};
+		int line_nums_{};
+		FFrame *previous_frame_{};
+	};
+
+	using ProcessInternalPrototype = void(__fastcall *)(UObject *calling_uobject,
+														FFrame &stack,
+														void *result);
+
+	void __fastcall ProcessInternalHook(UObject *calling_uobject,
+										FFrame &stack,
+										void *result);
+
+	inline ProcessInternalPrototype original_processinternal{};
+
+	using CallFunctionPrototype = void(__fastcall *)(UObject *calling_uobject,
+													 FFrame &stack,
+													 void *result, UFunction *calling_ufunction);
+
+	void __fastcall CallFunctionHook(UObject *calling_uobject,
+									 FFrame &stack,
+									 void *result, UFunction *calling_ufunction);
+
+	inline CallFunctionPrototype original_callfunction{};
+
+	inline UFunctionHooks<ProcessEventPrototype> processevent_hooks{};
+	inline UFunctionHooks<ProcessInternalPrototype> processinternal_hooks{};
+	inline UFunctionHooks<CallFunctionPrototype> callfunction_hooks{};
+}
+
+namespace UE5
+{
+	using ProcessEventPrototype = void(__fastcall *)(UObject *calling_uobject,
+													 UFunction *calling_ufunction,
+													 void *parameters);
+
+	void __fastcall ProcessEventHook(UObject *calling_uobject,
+									 UFunction *calling_ufunction,
+									 void *parameters);
+
+	inline ProcessEventPrototype original_processevent{};
+
+	class FFrame
+	{
+	public:
+		void *vmt_{};
+		char unknown_data_00_[8]{};
+		UStruct *node_{};
+		UObject *object_{};
+		unsigned char *code_{};
+		char *locals_{};
+		int line_nums_{};
+		FFrame *previous_frame_{};
+	};
+
+	using ProcessInternalPrototype = void(__fastcall *)(UObject *calling_uobject,
+														FFrame &stack,
+														void *result);
+
+	void __fastcall ProcessInternalHook(UObject *calling_uobject,
+										FFrame &stack,
+										void *result);
+
+	inline ProcessInternalPrototype original_processinternal{};
+
+	using CallFunctionPrototype = void(__fastcall *)(UObject *calling_uobject,
+													 FFrame &stack,
+													 void *result, UFunction *calling_ufunction);
+
+	void __fastcall CallFunctionHook(UObject *calling_uobject,
+									 FFrame &stack,
+									 void *result, UFunction *calling_ufunction);
+
+	inline CallFunctionPrototype original_callfunction{};
+
+	inline UFunctionHooks<ProcessEventPrototype> processevent_hooks{};
+	inline UFunctionHooks<ProcessInternalPrototype> processinternal_hooks{};
+	inline UFunctionHooks<CallFunctionPrototype> callfunction_hooks{};
+}
+
+#define UE3_PROCESSEVENT_HOOK(function_hook_name) void __fastcall function_hook_name(UObject *calling_uobject, void *unused, UFunction *calling_ufunction, void *parameters, void *result)
+#define UE3_PROCESSINTERNAL_HOOK(function_hook_name) void __fastcall function_hook_name(UObject *calling_uobject, void *unused, UE3::FFrame &stack, void *result)
+
+#define UE4_PROCESSEVENT_HOOK(function_hook_name) void __fastcall function_hook_name(UObject *calling_uobject, UFunction *calling_ufunction, void *parameters)
+
+#define UE5_PROCESSEVENT_HOOK(function_hook_name) void __fastcall function_hook_name(UObject *calling_uobject, UFunction *calling_ufunction, void *parameters)
+
+// TODO: Implement UE4 & UE5 macros for ProcessInternal
